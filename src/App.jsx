5e54521d8d4457c706c0b7370b1702c32119b239
singhshubham593 <92996831+singhshubham593@users.noreply.github.com>
2025-05-29 22:54:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './components/Home'
 
 function App() {
+  
 
   return (
     <>
      <Header />
-     <Outlet/>
+     { <Home /> && <Outlet />}
      <Footer />
     </>
   )
